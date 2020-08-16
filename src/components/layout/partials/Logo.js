74dc98c0ce6d192569fa362c1e2e97e55c7cc67a
Rishabh <ristripathi97@gmail.com>
2020-08-17 04:50:01 +0530

    Change site logo

--- src/components/layout/partials/Logo.js
+++ src/components/layout/partials/Logo.js
@@ -21,9 +21,9 @@ const Logo = ({
       <h1 className="m-0">
         <Link to="/">
           <Image
-            src={require('./../../../assets/images/logo.svg')}
+            src='/logo.svg'
             alt="Open"
-            width={32}
+            width={64}
             height={32} />
         </Link>
       </h1>
