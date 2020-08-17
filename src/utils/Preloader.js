bb6e0c1163364a815634cbf48c46de9ed0f93e0b
Rishabh <ristripathi97@gmail.com>
2020-08-17 17:53:06 +0530

    Changes in preloader

--- src/utils/Preloader.js
+++ src/utils/Preloader.js
@@ -3,7 +3,7 @@ import {FaHeart} from 'react-icons/fa'
 const Preloader = () => {
     return (
         <div class="preloader-container">
-            <img alt="logo" src="/logo-preloader.svg"></img>
+            <img alt="logo" src="/logo.svg"></img>
             <div>Made with <FaHeart class="icon"/> in India</div>
         </div>
     )
