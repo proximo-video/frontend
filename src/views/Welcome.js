9844bf344ccdc9572eaf248a215fa55e97a4f1ae
Rishabh <ristripathi97@gmail.com>
2020-08-18 08:50:51 +0530

    Add redirect after login

--- src/views/Welcome.js
+++ src/views/Welcome.js
@@ -1,15 +1,16 @@
-import React, { useEffect, useState } from 'react';
-import { Redirect } from 'react-router-dom';
+import React, { useEffect} from 'react';
 import { useDispatch } from 'react-redux';
 import { login,setId,setName,setRooms } from '../redux/actions';
-function Welcome() {
-    console.log(window.location.href);
-    const [success, setSuccess] = useState(false);
+function Welcome(props) {
     const dispatch = useDispatch();
     useEffect(() => {
         const queryString = window.location.search;
         const urlParams = new URLSearchParams(queryString);
         const code = urlParams.get("code");
+        const state = urlParams.get("state");
+        const redirectURL = new URLSearchParams(state).get("path");
+        console.log(redirectURL)
+        const noRedirect = ['/','/privacy-policy','/about-us']        
         const scope = urlParams.get("scope")
         let service;
         scope === null ? service = "github" : service = "google";
@@ -29,14 +30,17 @@ function Welcome() {
                 if (data.rooms)
                     dispatch(setRooms(data.rooms));
                 dispatch(login());
-                setSuccess(true);
+                if(noRedirect.indexOf(redirectURL)!==-1 || !redirectURL)
+                    props.history.push('/user');
+                else
+                    props.history.push(redirectURL)
             }
         }
         fetchData();
         // eslint-disable-next-line
     }, []);
 
-    return (success ? <Redirect to={{ pathname: "/user" }}></Redirect> : <></>)
+    return (<></>)
 
 }
 
