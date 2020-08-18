ee86f13719e6794e10458b756e7a5fe0646fb671
Aniket Singh <aniketsingh0104@gmail.com>
2020-08-18 15:38:31 +0530

    Add empty name check in room entry

--- src/views/RoomEntry.js
+++ src/views/RoomEntry.js
@@ -1,21 +1,31 @@
 import React from 'react';
 import Button from '../components/elements/Button';
-import Input from '../components/elements/Input'
 import PersonalMedia from './PersonalMedia';
 import {useDispatch, useSelector} from 'react-redux';
 import {setName} from '../redux/actions';
 import '../assets/scss/custom/roomEntry.scss';
-import { useHistory } from "react-router-dom";
+// import { useHistory } from "react-router-dom";
 
 const RoomEntry = (props) => {
     const isRoomOwner = useSelector((state) => state.isRoomOwner);
+    const name = useSelector((state) => state.name).trim();
     const dispatch = useDispatch();
-    const history = useHistory();
+    // console.log('name:', name);
+    // const history = useHistory();
     const nameInputHandler = (e) => {
-        dispatch(setName(e.target.value));
+        const name = e.target.value.trim();
+        if (name !== '')
+            dispatch(setName(e.target.value));
     }
     const onCancelButtonClick = () => {
 
+    }
+    const onJoinButtonClick = () => {
+        const name = document.getElementById('input-name').value.trim();
+        if (name !== '')
+            props.createSocket();
+        else
+            console.log('empty name not allowed');
     }
     return (
         <>
@@ -24,12 +34,12 @@ const RoomEntry = (props) => {
                     <div className="card-content">
                         <PersonalMedia mediaSuccess={props.mediaSuccess} setMediaSuccess={props.setMediaSuccess}/>
                         <div className="room-entry-form">
-                            {props.logged ? <></> : <input type="text" onChange={nameInputHandler} placeholder="Name" className={"room-entry-input"}/>}
+                            {props.logged && name !== '' ? <></> : <input required type="text" onChange={nameInputHandler} id="input-name" placeholder="Name" className={"room-entry-input"}/>}
                             <div className={"room-entry-buttons"}>
                                 <Button color="primary" wide className={"join-button"}
                                         disabled={!(props.mediaSuccess && props.iceSuccess)}
                                         onClick={props.createSocket}>{isRoomOwner ? 'Start' : 'Join'}</Button>
-                                <Button color="primary" wide className={"cancel-button"}
+                                <Button color="primary" onClick={onCancelButtonClick} wide className={"cancel-button"}
                                         onClick={null}>Cancel</Button>
                             </div>
                             {props.showWaiting && "Waiting"}
