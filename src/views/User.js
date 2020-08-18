756986910bae2747cd3e04ba32f11aca98bd5165
Aniket Singh <aniketsingh0104@gmail.com>
2020-08-19 02:54:54 +0530

    Change css of user.js and add loaders

--- src/views/User.js
+++ src/views/User.js
@@ -3,7 +3,6 @@ import { Link } from 'react-router-dom';
 import Switch from '../components/elements/Switch';
 import Button from '../components/elements/Button';
 import Modal from '../components/elements/Modal';
-import Input from '../components/elements/Input';
 import { FaTrash } from 'react-icons/fa';
 import { useDispatch, useSelector } from 'react-redux';
 import { setName, setRooms, setId } from '../redux/actions';
@@ -18,6 +17,8 @@ function User(props) {
     const [showModal, setShowModal] = useState(false);
     const [deleteRoomId, setDeleteRoomId] = useState("");
     const [roomIdInput, setRoomIdInput] = useState("");
+    const [showToggleLoader, setShowToggleLoader] = useState('');
+    const [showAddRoomLoader, setShowAddRoomLoader] = useState(false);
 
     const roomInputHandle = (event) => {
         setRoomIdInput(event.target.value)
@@ -41,9 +42,10 @@ function User(props) {
     const addRoom = async (e) => {
         const element = e.target;
         if (roomIdInput.match(/^[0-9a-zA-Z]+$/)) {
-            element.setAttribute("disabled", "true")
-            console.log(element)
-            console.log("Clicked")
+            setShowAddRoomLoader(true);
+            element.setAttribute("disabled", "true");
+            // console.log(element);
+            // console.log("Clicked");
             let response = await fetch('https://proximo-video.herokuapp.com/newRoom', {
                 method: 'POST',
                 credentials: 'include',
@@ -57,14 +59,16 @@ function User(props) {
                 fetchData();
             }
             element.removeAttribute("disabled");
+            setShowAddRoomLoader(false);
         } else{
-            alert("Enter numbers and alphabets only.")
+            alert("Enter numbers and alphabets only.");
         }
     }
 
     const toggleRoom = async (e) => {
         const element = e.target;
-        const id = element.getAttribute("roomid")
+        const id = element.getAttribute("roomid");
+        setShowToggleLoader(id);
         element.setAttribute("disabled", true);
         let response = await fetch('https://proximo-video.herokuapp.com/toggle', {
             method: 'POST',
@@ -78,6 +82,7 @@ function User(props) {
             fetchData();
         }
         element.removeAttribute("disabled");
+        setShowToggleLoader('');
     }
 
     const openDeleteModal = (e) => {
@@ -117,12 +122,9 @@ function User(props) {
             <Modal show={showModal} handleClose={closeDeleteModal}>
                 <div>
                     <h3>Warning</h3>
-
                     <p>{deleteRoomId} will be deleted</p>
-
                     <Button color="danger" onClick={deleteRoom} className="m-8">Delete</Button>
                     <Button onClick={closeDeleteModal} className="m-8">Cancel</Button>
-
                 </div>
             </Modal>
         )
@@ -135,12 +137,21 @@ function User(props) {
                     <div className="level">
                         <div className="level-left">
                             <span className="has-text-white-ter	">{value.room_id}</span>
-
                         </div>
                         <div className="level-right">
-                            <div className="margin-bottom-mobile m-16"><Switch roomid={value.room_id} className="has-text-white-ter mr-6" checked={value.is_locked || false} onChange={toggleRoom}>{value.is_locked ? "Locked" : "Open"}</Switch></div>
-                            <div className=" m-8"><Button color="primary"><Link to={{ pathname: "/" + value.room_id }}>Go To Room</Link></Button></div>
-                            <Button color="danger" className="ml-8" roomid={value.room_id} onClick={openDeleteModal}><FaTrash className="has-text-white" /></Button>
+                            <div className="margin-bottom-mobile m-16 room-lock-toggle">
+                                {
+                                    showToggleLoader === value.room_id ?
+                                        <div className="toggle-lock-loader"/>
+                                    :
+                                    <Switch roomid={value.room_id} className="has-text-white-ter mr-6"
+                                            checked={value.is_locked || false} onChange={toggleRoom}>
+                                        {value.is_locked ? "Locked" : "Open"}
+                                    </Switch>
+                                }
+                            </div>
+                            <div className=" m-8"><Button color="primary goto-room-button"><Link to={{ pathname: "/" + value.room_id }}>Go To Room</Link></Button></div>
+                            <Button color="danger delete-room-button" className="ml-8" roomid={value.room_id} onClick={openDeleteModal}><FaTrash className="has-text-white" /></Button>
                         </div>
                     </div>
                 </div>
@@ -149,18 +160,21 @@ function User(props) {
 
     return isLogged ? (
         <div className="section">
-            <div className="container">
-                <div className="h2 mb-32  ta-c">Welcome {name}</div>
-                <div className="columns">
-                    <div className="column is-four-fifths">
-                        <Input value={roomIdInput} onChange={roomInputHandle}></Input>
-                    </div>
-                    <div className="column ta-c">
-                        <Button color="primary" onClick={addRoom}>Add Room</Button>
-                    </div>
+            <div className="container user-rooms">
+                {/*<div className="h2 mb-32  ta-c">Welcome {name}</div>*/}
+                <h2>Welcome {name}</h2>
+                <h6>Create your private rooms here.</h6>
+                <p>*At max 3 private rooms are allowed per user.</p>
+                <div className="input-area">
+                    <input className={"form-input"} value={roomIdInput} onChange={roomInputHandle} placeholder={"Room Name"}/>
+                    <Button color="primary add-room-button" onClick={addRoom}>Create Room</Button>
                     {showDeleteModal()}
                 </div>
-                {rooms ? rooms.map((value, key) => genRooms(value, key)) : <></>}
+                <div className={"rooms"}>
+                    {showAddRoomLoader && <><div className="add-room-loader"/><h6>Creating room. Please wait.</h6></>}
+                    <p>{rooms.length === 0 ? 'Your rooms will be displayed here.' : 'These are your rooms.'}</p>
+                    {rooms ? rooms.map((value, key) => genRooms(value, key)) : <></>}
+                </div>
             </div>
         </div>
     ) : <></>;
