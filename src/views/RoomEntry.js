7af59d25d540da7d7fe91a9b1ff0b721cece78ad
Aniket Singh <aniketsingh0104@gmail.com>
2020-08-18 14:57:50 +0530

    Adjust some css in room entry

--- src/views/RoomEntry.js
+++ src/views/RoomEntry.js
@@ -2,14 +2,14 @@ import React from 'react';
 import Button from '../components/elements/Button';
 import Input from '../components/elements/Input'
 import PersonalMedia from './PersonalMedia';
-import {useDispatch,useSelector} from 'react-redux';
+import {useDispatch, useSelector} from 'react-redux';
 import {setName} from '../redux/actions';
 import '../assets/scss/custom/roomEntry.scss';
 
 const RoomEntry = (props) => {
-    const isRoomOwner  = useSelector((state)=>state.isRoomOwner);
+    const isRoomOwner = useSelector((state) => state.isRoomOwner);
     const dispatch = useDispatch();
-    const nameInputHandler = (e)=>{
+    const nameInputHandler = (e) => {
         dispatch(setName(e.target.value));
     }
     return (
@@ -19,9 +19,15 @@ const RoomEntry = (props) => {
                     <div className="card-content">
                         <PersonalMedia mediaSuccess={props.mediaSuccess} setMediaSuccess={props.setMediaSuccess}/>
                         <div className="room-entry-form">
-                            {props.logged?<></>:<Input onChange={nameInputHandler} placeholder="Name" className="input mt-32"/>}
-                            <Button color="primary" wide className={"mt-32 join-button"} disabled={!(props.mediaSuccess && props.iceSuccess)} onClick={props.createSocket}>{isRoomOwner?'Start':'Join'}</Button>
-                            {props.showWaiting&&"Waiting"}
+                            {props.logged ? <></> : <input type="text" onChange={nameInputHandler} placeholder="Name" className={"room-entry-input"}/>}
+                            <div className={"room-entry-buttons"}>
+                                <Button color="primary" wide className={"mt-32 join-button"}
+                                        disabled={!(props.mediaSuccess && props.iceSuccess)}
+                                        onClick={props.createSocket}>{isRoomOwner ? 'Start' : 'Join'}</Button>
+                                <Button color="primary" wide className={"mt-32 cancel-button"}
+                                        onClick={null}>Cancel</Button>
+                            </div>
+                            {props.showWaiting && "Waiting"}
                         </div>
                     </div>
                 </div>
