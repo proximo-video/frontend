41d0a119ab54e206be7bb6f73b8d87db5f4862f5
Rishabh <ristripathi97@gmail.com>
2020-08-19 20:31:05 +0530

    Redirect in login page if already logged in

--- src/views/SignIn.js
+++ src/views/SignIn.js
@@ -3,11 +3,16 @@ import ButtonGroup from '../components/elements/ButtonGroup';
 import Button from '../components/elements/Button';
 import GoogleLogo from '../assets/images/google.png';
 import GithubLogo from '../assets/images/github.png';
+import { useSelector } from 'react-redux';
+import { Redirect } from "react-router-dom";
 
 const SignIn = (props) => {
-    const state ='path='+props.location.state.prevPath;
-    console.log(props.location.state.prevPath)
-    return (
+    const isLogged = useSelector(state => state.isLogged);
+    let path = '';
+    if (props.location.state)
+        path = props.location.state.prevPath
+    const state = 'path=' + path;
+    return (!isLogged ?
         <div className="container section">
             <ButtonGroup>
                 <Button tag="a" color="dark" wideMobile href={"https://accounts.google.com/o/oauth2/v2/auth?client_id=287838666978-fnnt6fujf4malkfn3ppoqrrdcfqk75h1.apps.googleusercontent.com&redirect_uri=http://localhost:8000/welcome.html&response_type=code&scope=profile&state=" + state}>
@@ -17,7 +22,7 @@ const SignIn = (props) => {
                     Login with <img className="logo" src={GithubLogo} alt="Github Login"></img>
                 </Button>
             </ButtonGroup>
-        </div>
+        </div> : <Redirect to='/user' />
     )
 }
 
