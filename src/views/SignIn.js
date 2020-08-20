c428f1197fa10506734af0b0a81739b51a8dc07f
Aniket Singh <aniketsingh0104@gmail.com>
2020-08-20 16:04:27 +0530

    Login page complete

--- src/views/SignIn.js
+++ src/views/SignIn.js
@@ -13,15 +13,22 @@ const SignIn = (props) => {
         path = props.location.state.prevPath
     const state = 'path=' + path;
     return (!isLogged ?
-        <div className="container section">
-            <ButtonGroup>
-                <Button tag="a" color="dark" wideMobile href={"https://accounts.google.com/o/oauth2/v2/auth?client_id=287838666978-fnnt6fujf4malkfn3ppoqrrdcfqk75h1.apps.googleusercontent.com&redirect_uri=http://localhost:8000/welcome.html&response_type=code&scope=profile&state=" + state}>
-                    Login with <img className="logo" src={GoogleLogo} alt="Google Login"></img>
-                </Button>
-                <Button tag="a" wideMobile href={"https://github.com/login/oauth/authorize?client_id=a3b7842c5f4c93cc4b7d&redirect_uri=https://proximo.pw/welcome&state=" + state}>
-                    Login with <img className="logo" src={GithubLogo} alt="Github Login"></img>
-                </Button>
-            </ButtonGroup>
+        <div className="login-container">
+            <div className={"card login-container-inner"}>
+                <div className={"card-content"}>
+                    <h1 className={"login-h1"}>Login</h1>
+                    <h6 className={"login-h6"}>To experience some awesomeness.</h6>
+                    <p className={"login-medium"}>We are committed to deliver quality experience.</p>
+                    <ButtonGroup>
+                        <Button tag="a" color="dark" wideMobile href={"https://accounts.google.com/o/oauth2/v2/auth?client_id=287838666978-fnnt6fujf4malkfn3ppoqrrdcfqk75h1.apps.googleusercontent.com&redirect_uri=http://localhost:8000/welcome.html&response_type=code&scope=profile&state=" + state}>
+                            Login with <img className="logo" src={GoogleLogo} alt="Google Login"></img>
+                        </Button>
+                        <Button tag="a" wideMobile href={"https://github.com/login/oauth/authorize?client_id=a3b7842c5f4c93cc4b7d&redirect_uri=https://proximo.pw/welcome&state=" + state}>
+                            Login with <img className="logo" src={GithubLogo} alt="Github Login"></img>
+                        </Button>
+                    </ButtonGroup>
+                </div>
+            </div>
         </div> : <Redirect to='/user' />
     )
 }
