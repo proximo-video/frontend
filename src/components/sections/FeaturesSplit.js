1579e6256fb4fe880e957b8c85368c231f6a5547
Aniket Singh <aniketsingh0104@gmail.com>
2020-08-20 11:21:23 +0530

    Home page middle image section css complete

--- src/components/sections/FeaturesSplit.js
+++ src/components/sections/FeaturesSplit.js
@@ -82,7 +82,7 @@ const FeaturesSplit = ({
                 )}
                 data-reveal-container=".split-item">
                 <Image
-                  src={'/images/simpleUrl.jpg'}
+                  src={'/images/simple_url.jpg'}
                   alt="Simple room url"
                   width={528}
                   height={396} />
@@ -98,7 +98,7 @@ const FeaturesSplit = ({
                   Dark theme UI.
                   </h3>
                 <p className="m-0">
-                  Collaborate with your team or friends. Experience amazing dark mode UI. With HD video calls.
+                  Collaborate with your team or friends. Experience amazing simple but robust UI. With quality video calls.
                   </p>
               </div>
               <div className={
@@ -139,6 +139,32 @@ const FeaturesSplit = ({
                   width={700}
                   height={450}
                 />
+              </div>
+            </div>
+
+            <div className="split-item">
+              <div className="split-item-content center-content-mobile reveal-from-right" data-reveal-container=".split-item">
+                <div className="text-xxs text-color-primary fw-600 tt-u mb-8">
+                  User page
+                </div>
+                <h3 className="mt-0 mb-12">
+                  Multiple private rooms.
+                </h3>
+                <p className="m-0">
+                  Create multiple private rooms to host different meetings.
+                </p>
+              </div>
+              <div className={
+                classNames(
+                    'split-item-image center-content-mobile reveal-from-bottom',
+                    imageFill && 'split-item-image-fill'
+                )}
+                   data-reveal-container=".split-item">
+                <Image
+                    src={'/images/user_page.jpg'}
+                    alt="Features split 02"
+                    width={528}
+                    height={396} />
               </div>
             </div>
 
