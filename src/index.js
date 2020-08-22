7bf0bbfa225e753ef581ea69d8db2e1223fddd38
Rishabh <ristripathi97@gmail.com>
2020-08-22 07:35:00 +0530

    Sentry Integration

--- src/index.js
+++ src/index.js
@@ -5,6 +5,8 @@ import { createBrowserHistory } from "history";
 import { createStore, applyMiddleware, compose } from 'redux';
 import allReducers from './redux/reducers';
 import App from './App';
+import * as Sentry from '@sentry/react';
+import { Integrations } from '@sentry/apm';
 import * as serviceWorker from './serviceWorker';
 import { Provider } from 'react-redux';
 // eslint-disable-next-line
@@ -15,12 +17,21 @@ import './assets/scss/style.scss';
 import getUserMediaMiddleware from './middleware/getUserMedia';
 import webRTCMiddleware from './middleware/webRTC';
 import ScrollToTop from './utils/ScrollToTop';
+Sentry.init({
+  dsn: "https://a2b90a79a3a24407b5378d0cb7cb3ac4@o437482.ingest.sentry.io/5400169",
+  integrations: [
+    new Integrations.Tracing(),
+  ],
+  tracesSampleRate: 0.1,
+});
+const sentryReduxEnhancer = Sentry.createReduxEnhancer({
+});
 let store;
-if (process.env.NODE_ENV === 'development'){
+if (process.env.NODE_ENV === 'development') {
   store = createStore(allReducers, compose(applyMiddleware(getUserMediaMiddleware, webRTCMiddleware), window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()));
 }
-else{
-  store = createStore(allReducers, compose(applyMiddleware(getUserMediaMiddleware, webRTCMiddleware)));
+else {
+  store = createStore(allReducers, compose(applyMiddleware(getUserMediaMiddleware, webRTCMiddleware), sentryReduxEnhancer));
 }
 
 const history = createBrowserHistory();
@@ -28,7 +39,7 @@ const history = createBrowserHistory();
 ReactDOM.render(
   <Provider store={store}>
     <Router history={history}>
-      <ScrollToTop/>
+      <ScrollToTop />
       <App />
     </Router>
   </Provider>,
