347b9e8e468c5402e7f870697327667c836f9529
Aniket Singh <aniketsingh0104@gmail.com>
2020-08-22 16:01:46 +0530

    Add form in add room input

--- src/views/User.js
+++ src/views/User.js
@@ -253,15 +253,17 @@ function User(props) {
                     <label className={"input-label"} style={showRoomNameWarning ? { display: 'block' } : {}}>
                         Room name can only be alphanumeric:
                     </label>
-                    <input
-                        id={"room-name-input"}
-                        className={"form-input"}
-                        value={roomIdInput}
-                        onChange={roomInputHandle}
-                        placeholder={"Room Name"}
-                        style={showRoomNameWarning ? { border: '3px solid #f26b4c' } : {}}
-                    />
-                    <Button color="primary add-room-button" onClick={addRoom}>Create Room</Button>
+                    <form onSubmit={addRoom} className={"input-area-form"}>
+                        <input
+                            id={"room-name-input"}
+                            className={"form-input"}
+                            value={roomIdInput}
+                            onChange={roomInputHandle}
+                            placeholder={"Room Name"}
+                            style={showRoomNameWarning ? { border: '3px solid #f26b4c' } : {}}
+                        />
+                        <Button color="primary add-room-button" onClick={addRoom}>Create Room</Button>
+                    </form>
                     {showDeleteModal()}
                 </div>
                 <div className={"rooms"}>
