e6221ed1f6f9dd417164d8cd70a455df84c596d1
Rishabh <ristripathi97@gmail.com>
2020-11-14 17:49:02 +0530

    Google one tap login

--- src/views/Home.js
+++ src/views/Home.js
@@ -10,23 +10,12 @@ import { AboutUsInner } from "./AboutUs";
 // import Button from "../components/elements/Button";
 // import Modal from "../components/elements/Modal";
 
-const oneTapLogin = () => {
-    return (
-        <div id="g_id_onload"
-            data-client_id="150668394436-t4sh915ilqum6t7a8lf0i4p42ilkg0s3.apps.googleusercontent.com"
-            data-callback="authenticationResponse"
-            data-login_uri="https://proximo.pw/welcome">
-        </div>
-    )
-}
-
 const Home = (props) => {
     const isLogged = useSelector(state => state.isLogged);
     if (isLogged)
         props.history.push('/user');
     return (
         <>
-            {oneTapLogin()}
             <Hero history={props.history} className="illustration-section-01" />
             {/*<FeaturesTiles />*/}
             <FeaturesSplit invertMobile topDivider imageFill className="illustration-section-02" />
